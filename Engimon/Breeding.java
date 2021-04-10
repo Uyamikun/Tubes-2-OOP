6995adf46a25c600f5b1ca80847bec8b55443669
Uyamikun <nabil.nabighah@gmail.com>
2021-04-10 13:14:38 +0700

    add getAdvantage from Element1 and Element2

--- Engimon/Breeding.java
+++ Engimon/Breeding.java
@@ -1,8 +1,9 @@
 package Engimon;
 import java.util.*;
 public class Breeding {
+    //MATRIX ADVANTAGE [FIRE,WATER,ELECTRIC,GROUND,ICE] sama kebawahnya juga gini
     private static Double[][] matrixAdvantage = {{1.0,0.0,1.0,0.5,2.0},{2.0,1.0,0.0,1.0,1.0},{1.0,2.0,1.0,0.0,1.5},{1.5,1.0,2.0,1.0,0.0},{0.0,1.0,0.5,2.0,1.0}};
-
+    private static ArrayList<String> arrayElemenString = new ArrayList<String>(){{add("Fire");add("Water");add("Electric");add("Ground");add("Ice");}};
 //================= METHOD ========================
     // Mengembalikan index dari skill yang memiliki mastery level tertinggi
     public Skill getMaxMasterySkill(ArrayList<Skill> ESkill){
@@ -20,12 +21,22 @@ public class Breeding {
         return s;
     }
  
-    // ArrayList<Skill> InheritSkill(ArrayList<Skill> ASkill, ArrayList<Skill> BSkill){}
+    // ArrayList<Skill> InheritSkill(ArrayList<Skill> ASkill, ArrayList<Skill> BSkill){
+
+    // }
  
-    // String getIndex(ArrayList<String> _list, int _i){}
- 
-    // int getAdvantage(String A, String B); // Mengembalikan advantage A terhadap B
- 
+    // String getIndex(ArrayList<String> _list, int _i){
+        
+    // }
+    int getIndexElement(String elemen){
+        return Breeding.arrayElemenString.indexOf(elemen);
+    }
+    Double getAdvantage(String A, String B){
+        int elemen1 = getIndexElement(A);
+        int elemen2 = getIndexElement(B);
+        return Breeding.matrixAdvantage[elemen1][elemen2];
+    } // Mengembalikan advantage A terhadap B
+    
     // bool IsElementCompatible(ArrayList<String> ElementA, ArrayList<ArrayList<String>> all); // mengecek apakah list string ada pada combinasi dual yang mungkin
  
     // String CompareElementByAdvantage(String elementA, String elementB);
